2197c2611d2edb1b9972fe2cfec909ce3d19c2ca
Enzo Henrique Favaro <enzo.h.favaro@gmail.com>
2025-04-09 20:07:00 -0300

    MediaAluno.java

--- MediaAluno.java
+++ MediaAluno.java
@@ -4,23 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner teclado = new Scanner(System.in);
+		Scanner entrada = new Scanner(System.in);
 		int aluno, i=0;
 		String nome;
 		
 		System.out.printf("Digite a quantidade de alunos:" );
-		aluno = teclado.nextInt();
+		aluno = entrada.nextInt();
 		
 		for(i=0;i<aluno;i++) {
 			
 			System.out.print("Digite o nome do aluno: ");
-			nome = teclado.next();
+			nome = entrada.next();
 			
 			System.out.print("Digite a nota da prova 1: ");
-			float n1 = teclado.nextFloat();
+			float n1 = entrada.nextFloat();
 			
 			System.out.print("Digite a nota da prova 2: ");
-			float n2 = teclado.nextFloat();
+			float n2 = entrada.nextFloat();
 			
 			float media = (n1+n2)/2;
 			System.out.printf("Media de %s: %.2f\n",nome, media);
