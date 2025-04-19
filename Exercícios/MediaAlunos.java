17e359829e16d268b8271886ec46df2ab9db1a9b
Enzo Henrique Favaro <enzo.h.favaro@gmail.com>
2025-04-19 17:45:55 -0300

    MediaAlunos.java

---
+++ Exercícios/MediaAlunos.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		int aluno, i = 0;
+		String nome;
+
+		System.out.printf("Digite a quantidade de alunos:");
+		aluno = entrada.nextInt();
+
+		for (i = 0; i < aluno; i++) {
+
+			System.out.print("Digite o nome do aluno: ");
+			nome = entrada.next();
+
+			System.out.print("Digite a nota da prova 1: ");
+			float n1 = entrada.nextFloat();
+
+			System.out.print("Digite a nota da prova 2: ");
+			float n2 = entrada.nextFloat();
+
+			float media = (n1 + n2) / 2;
+			System.out.printf("Media de %s: %.2f\n", nome, media);
+
+			if (media < 6) {
+				System.out.println("Aluno(a) Reprovado(a)!\n");
+			} 
+			
+			else {
+				System.out.println("Aluno(a) Aprovado(a)!\n");
+			}
+
+		}
+
+	}
+
+}
