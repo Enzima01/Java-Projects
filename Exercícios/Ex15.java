f2cbd2f1cb7da758af1758f77cf716968661acef
Enzo Henrique Favaro <enzo.h.favaro@gmail.com>
2025-04-20 13:15:55 -0300

    Add files via upload

---
+++ Exercícios/Ex15.java
@@ -0,0 +1,27 @@
+/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.*/
+
+import java.util.Scanner;
+
+public class Ex15 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("-----------------");
+		System.out.print("Digite sua senha: ");
+		int senha = sc.nextInt();
+
+		while (senha != 2002) {
+			System.out.println("Senha Invalida!");
+			System.out.println("-----------------");
+			System.out.print("Digite sua senha: ");
+			senha = sc.nextInt();
+		}
+
+		System.out.println("Acesso Permitido!");
+	}
+
+}
